1a9b071c2a9f7d5614fe76950c72f1893d03bfc7
Alyssa Denary <alyssa.denary@gmail.com>
2023-03-20 15:33:16 -0400

    add serialization method

--- models.py
+++ models.py
@@ -49,3 +49,14 @@ class Cupcake(db.Model):
         nullable=False,
         default=DEFAULT_IMAGE_URL
     )
+
+    def serialize(self):
+        """Serialize to dictionary"""
+
+        return {
+            "id":self.id,
+            "flavor":self.flavor,
+            "size":self.size,
+            "rating":self.rating,
+            "image":self.image
+        }
